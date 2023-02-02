0d869e44c114c77c5da45d11bc67d84b494b958b
henriquegouveia1 <henriquegs002@gmail.com>
2023-02-02 11:59:15 -0300

    iniciando nimigo

---
+++ Assets/Scripts/Enemies/EnemieMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float patrolTime = 3f;
+    public bool facingRight = false;
+    public Rigidbody2D rb;
+    public GameObject Enemie;
+
+    void Start()
+    {
+        Enemie = GameObject.FindWithTag("Enemie");
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+        
+    }
+
+    void Update()
+    {
+        Walk();
+    }
+
+    public void Walk()
+    {
+
+        if (!facingRight)
+        {
+            Vector3 move = new Vector3(-1 * speed, rb.velocity.y, 0f);
+            rb.velocity = move;
+
+            patrolTime -= Time.deltaTime;
+            if (patrolTime <= 0)
+                facingRight = true;
+        }
+        if (facingRight)
+        {
+            Vector3 move = new Vector3(1 * speed, rb.velocity.y, 0f);
+            rb.velocity = move;
+
+            patrolTime -= Time.deltaTime;
+            if (patrolTime <= 0)
+                facingRight = false;
+        }
+    }
+
+}
